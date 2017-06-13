783642e7844042c0f7a79e885a7955066d71ae3a
jhmullen <jimmy.mullen@gmail.com>
2017-06-13 10:01:57 -0400

    hooks up links to actual pages via router

--- app/routes.jsx
+++ app/routes.jsx
@@ -3,12 +3,18 @@ import {Route, IndexRoute} from "react-router";
 
 import App from "components/App";
 import Home from "pages/Home";
+import Lesson from "pages/Lesson";
+import Glossary from "pages/Glossary";
+import Profile from "pages/Profile";
 
 export default function RouteCreate() {
 
   return (
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
+      <Route path="lesson" component={Lesson} />
+      <Route path="glossary" component={Glossary} />
+      <Route path="profile" component={Profile} />
     </Route>
   );
 }

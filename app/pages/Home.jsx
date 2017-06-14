6c2cb5896247aa547cbf70a2aa2409b57308cf73
jhmullen <jimmy.mullen@gmail.com>
2017-06-14 11:56:07 -0400

    adds test icon to homepage

--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -10,7 +10,7 @@ class Home extends Component {
 
     return (
       <div>
-        <h1>Codelife</h1>
+        <h1><img className="image" width="40" height="40" src="icon.svg" />&nbsp;Codelife</h1>
         <ul>
           <li><Link className="link" to="/track">{ t("Tracks") }</Link></li>
           <li><Link className="link" to="/glossary">{ t("Glossary") }</Link></li>

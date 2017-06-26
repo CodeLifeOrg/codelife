be0c71e9387ed057d945406130802dc985e65888
jhmullen <jimmy.mullen@gmail.com>
2017-06-26 14:39:30 -0400

    adds rudimentary drag and drop experiment to studio page

---
+++ app/components/Dustbin.jsx
@@ -0,0 +1,61 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import {DropTarget} from "react-dnd";
+
+const style = {
+  height: "12rem",
+  width: "12rem",
+  marginRight: "1.5rem",
+  marginBottom: "1.5rem",
+  color: "white",
+  padding: "1rem",
+  textAlign: "center",
+  fontSize: "1rem",
+  lineHeight: "normal",
+  float: "left"
+};
+
+const boxTarget = {
+  drop() {
+    return {dropper: "Dustbin"};
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+  };
+}
+
+class Dustbin extends Component {
+
+  render() {
+    const {canDrop, isOver, connectDropTarget} = this.props;
+    const isActive = canDrop && isOver;
+
+    let backgroundColor = "#222";
+    if (isActive) {
+      backgroundColor = "darkgreen";
+    } 
+    else if (canDrop) {
+      backgroundColor = "darkkhaki";
+    }
+
+    return connectDropTarget(
+      <div style={{...style, backgroundColor}}>
+        {isActive ? "Release to drop" : "Drag a box here" }
+      </div>
+    );
+  }
+}
+
+Dustbin.propTypes = {
+  connectDropTarget: PropTypes.func.isRequired,
+  isOver: PropTypes.bool.isRequired,
+  canDrop: PropTypes.bool.isRequired
+};
+
+
+export default DropTarget("snippet", boxTarget, collect)(Dustbin);
--- app/pages/Studio.jsx
+++ app/pages/Studio.jsx
@@ -1,6 +1,11 @@
 import React, {Component} from "react";
 import {translate} from "react-i18next";
 import Nav from "components/Nav";
+import {DragDropContextProvider} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import Snippet from "components/Snippet";
+import Dustbin from "components/Dustbin";
+
 //import AceEditor from "react-ace";
 
 //import "brace/mode/html";
@@ -49,21 +54,35 @@ class Studio extends Component {
     const {t} = this.props;
 
     return (
-      <div>
-        <h1>{ t("Studio") }</h1>
-        <button onClick={this.onClick.bind(this)}>Inject</button><br/><br/>       
-        <div style={{width:"1200px"}}>
-          <div style={{float:"left", width:"450px"}}>
-          { this.state.mounted ? <Editor ref={ comp => this.editor = comp } mode="html" theme="monokai" onChange={this.onChange.bind(this)} value={this.state.output}/> : null }
+      <DragDropContextProvider backend={HTML5Backend}>
+        <div>
+          <h1>{ t("Studio") }</h1>
+          <div style={{width:"1200px"}}>
+            <div style={{float:"left", width:"450px"}}>
+            <Snippet name="title snippet" text="<h1>title</h1>"/>
+            <Snippet name="list snippet" text="<ul><li>test</li></ul>"/>
+            <Snippet name="img snippet" text="<img src='img' />" />
+            </div>
+            <div style={{float:"right", width:"450px"}}>
+            <Dustbin />
+            </div>
           </div>
-          <div style={{float:"right", border:"solid 1px black", width: "650px", height: "400px"}} dangerouslySetInnerHTML={ {__html: this.state.output } } />
+          <div style={{clear:"both"}}></div>
+          <hr/>
+          <button onClick={this.onClick.bind(this)}>Inject</button><br/><br/>       
+          <div style={{width:"1200px"}}>
+            <div style={{float:"left", width:"450px"}}>
+            { this.state.mounted ? <Editor ref={ comp => this.editor = comp } mode="html" theme="monokai" onChange={this.onChange.bind(this)} value={this.state.output}/> : null }
+            </div>
+            <div style={{float:"right", border:"solid 1px black", width: "650px", height: "400px"}} dangerouslySetInnerHTML={ {__html: this.state.output } } />
+          </div>
+          <div style={{clear:"both"}}>
+          <br/><br/>
+          
+          <Nav />
+          </div>
         </div>
-        <div style={{clear:"both"}}>
-        
-        <Nav />
-        </div>
-      </div>
-
+      </DragDropContextProvider>
     );
   }
 }

7c2e74b9ed4509fc9ac7c4dfb653ec6d0d9e333f
Dave Landry <landry.dave@gmail.com>
2017-07-25 12:13:47 -0400

    adds "Loading..." text to localization

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -1,19 +1,23 @@
 import React, {Component} from "react";
 import {Intent, Spinner} from "@blueprintjs/core";
+import {translate} from "react-i18next";
 
 import "./Loading.css";
 
-export default class Loading extends Component {
+class Loading extends Component {
 
   render() {
+    const {t} = this.props;
 
     return (
       <div id="loading">
         <Spinner intent={Intent.WARNING} />
-        <h1 className="title">Loading...</h1>
+        <h1 className="title">{ t("Loading...") }</h1>
       </div>
     );
 
   }
 
 }
+
+export default translate()(Loading);

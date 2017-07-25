af0fb1786d719534a8f686d09f3f378c71d6bd05
Dave Landry <landry.dave@gmail.com>
2017-07-25 12:03:04 -0400

    designs Minilesson component (closes #9)

--- app/pages/Minilesson.jsx
+++ app/pages/Minilesson.jsx
@@ -3,8 +3,10 @@ import {connect} from "react-redux";
 import {Link} from "react-router";
 import React, {Component} from "react";
 import {translate} from "react-i18next";
-import {Button, Dialog, Intent, Popover} from "@blueprintjs/core";
+import {Button, Dialog, Intent, Tooltip} from "@blueprintjs/core";
 import "./Minilesson.css";
+
+import Loading from "components/Loading";
 
 class Minilesson extends Component {
 
@@ -28,9 +30,9 @@ class Minilesson extends Component {
 
     Promise.all([mlget, lget, uget, osget]).then(resp => {
       this.setState({
-        minilessons: resp[0].data, 
-        currentLesson: resp[1].data[0], 
-        userProgress: resp[2].data, 
+        minilessons: resp[0].data,
+        currentLesson: resp[1].data[0],
+        userProgress: resp[2].data,
         otherSnippets: resp[3].data
       });
     });
@@ -61,7 +63,7 @@ class Minilesson extends Component {
           <iframe className="snippetrender" frameBorder="0" ref={ comp => this.iframes[i] = comp } />
         </div>
         {!done ? <div className="finish-text">Complete this island to view source from other students!</div> : null}
-      </div>     
+      </div>
     );
   }
 
@@ -82,20 +84,29 @@ class Minilesson extends Component {
   }
 
   buildButton(snippet, i) {
+    const {t} = this.props;
     return (
-      <div>
-        <Button onClick={this.toggleDialog.bind(this, i)} text={`${snippet.username}: ${snippet.snippetname}`} />
+      <div className="snippet-link">
+        <div className="box" onClick={this.toggleDialog.bind(this, i)}>
+          <div className="snippet-title">{ snippet.snippetname }</div>
+          <div className="author">{ t("created by") } { snippet.username }</div>
+        </div>
         <Dialog
-          iconName="inbox"
           isOpen={this.state[`isOpen_${i}`]}
           onClose={this.toggleDialog.bind(this, i)}
-          title={`${snippet.username}: ${snippet.snippetname}`}
+          title={ snippet.snippetname }
           lazy={false}
           inline={true}
-          style={{width: "800px"}}
+          style={{
+            "height": "75vh",
+            "max-height": "600px",
+            "max-width": "800px",
+            "width": "100%"
+          }}
         >
           <div className="pt-dialog-body">{snippet ? this.buildWindow(i, snippet.studentcontent) : null}</div>
           <div className="pt-dialog-footer">
+            <div className="pt-dialog-footer-byline">{ t("created by") } { snippet.username }</div>
             <div className="pt-dialog-footer-actions">
               <Button
                 intent={Intent.PRIMARY}
@@ -109,23 +120,13 @@ class Minilesson extends Component {
     );
   }
 
-  buildLink(minilesson) {
-    const {lid} = this.props.params;
-    let css = "ml_link";
-    if (minilesson.isNext) css += " next";
-    if (minilesson.isDone) css += " completed";
-    return (
-      <Link className={css} to={`/lesson/${lid}/${minilesson.id}`}>{ minilesson.name }</Link>
-    ); 
-  }
-
   render() {
 
     const {t} = this.props;
     const {lid} = this.props.params;
     const {minilessons, currentLesson, userProgress, otherSnippets} = this.state;
 
-    if (!currentLesson || !minilessons || !userProgress || !otherSnippets) return <h1>Loading...</h1>;
+    if (!currentLesson || !minilessons || !userProgress || !otherSnippets) return <Loading />;
 
     // clone minilessons as to not mess with state
     const minilessonStatuses = minilessons.slice(0);
@@ -134,25 +135,46 @@ class Minilesson extends Component {
       minilessonStatuses[m].isDone = done;
       // if i'm the first lesson and i'm not done, i'm next lesson
       // if i'm past the first lesson and i'm not done but my previous one is, i'm the next lesson
-      minilessonStatuses[m].isNext = (m === 0 && !done) || (m > 0 && !done && minilessonStatuses[m - 1].isDone);
+      minilessonStatuses[m].isNext = m === 0 && !done || m > 0 && !done && minilessonStatuses[m - 1].isDone;
     }
 
-    const minilessonItems = minilessonStatuses.map(minilesson =>
-      <li>{this.buildLink.bind(this)(minilesson)}</li>);
+    const minilessonItems = minilessonStatuses.map(minilesson => {
+      const {lid} = this.props.params;
+      if (minilesson.isNext || minilesson.isDone) {
+        let css = "stop";
+        if (minilesson.isNext) css += " next";
+        else if (minilesson.isDone) css += " done";
+        return <Tooltip content={ minilesson.name } tooltipClassName={ currentLesson.id }>
+          <Link className={css} to={`/lesson/${lid}/${minilesson.id}`}></Link>
+        </Tooltip>;
+      }
+      return <div className="stop"></div>;
+    });
 
-    const otherSnippetItems = otherSnippets.map((os, i) =>
-      <li>{this.buildButton.bind(this)(os, i)}</li>);
+    const otherSnippetItems = otherSnippets.map((os, i) => this.buildButton.bind(this)(os, i));
 
     this.iframes = new Array(otherSnippets.length);
 
     return (
-      <div>
-        <h1>{currentLesson.name}</h1>
-        <p>{currentLesson.description}</p>
-        <ul>{minilessonItems}</ul>
-        <ul><li><Link className="editor-link" to={`/editor/${lid}`}>Snippet Boss Castle</Link></li></ul>
-        <strong>Other Snippets</strong><br/>
-        <ul>{otherSnippetItems}</ul>
+      <div id="island" className={ currentLesson.id }>
+        <div className="image">
+          <h1 className="title">{ currentLesson.name }</h1>
+          <p className="description">{ currentLesson.description }</p>
+          <div id="path">
+            { minilessonItems }
+            <Tooltip content="Final Test" tooltipClassName={ currentLesson.id }>
+              <Link className="stop editor-link" to={`/editor/${lid}`}></Link>
+            </Tooltip>
+          </div>
+        </div>
+        { otherSnippets.length
+        ? <div>
+            <h2 className="title">Other Students Snippets</h2>
+            <div id="snippets">
+              { otherSnippetItems }
+            </div>
+          </div>
+        : null }
       </div>
     );
   }

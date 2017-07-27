a9448f1b0c90fe586407500d626a4841b4ae95e2
jhmullen <jimmy.mullen@gmail.com>
2017-07-27 11:53:51 -0400

    breaks out validation rules, working on #28

--- app/components/CodeBlock.jsx
+++ app/components/CodeBlock.jsx
@@ -5,7 +5,7 @@ import React, {Component} from "react";
 import {translate} from "react-i18next";
 import himalaya from "himalaya";
 import AceWrapper from "components/AceWrapper";
-import {Intent, Position, Toaster} from "@blueprintjs/core";
+import {Intent, Position, Toaster, Alert} from "@blueprintjs/core";
 import "./CodeBlock.css";
 
 import Loading from "components/Loading";
@@ -17,12 +17,16 @@ class CodeBlock extends Component {
     this.state = {
       mounted: false,
       currentText: "",
-      isPassing: false
+      isPassing: false,
+      isOpen: false,
+      rulejson: null
     };
   }
 
   componentDidMount() {
-    this.setState({mounted: true, currentText: this.props.lesson.snippet.studentcontent}, this.renderText.bind(this));
+    const rulejson = JSON.parse(this.props.lesson.rulejson);
+    const currentText = this.props.lesson.snippet.studentcontent;
+    this.setState({mounted: true, currentText, rulejson}, this.renderText.bind(this));
   }
 
   getEditor() {
@@ -34,10 +38,17 @@ class CodeBlock extends Component {
   }
 
   renderText() {
-    const doc = this.refs.rc.contentWindow.document;
-    doc.open();
-    doc.write(this.state.currentText);
-    doc.close();
+    this.checkForErrors();
+    if (this.refs.rc) {
+      const doc = this.refs.rc.contentWindow.document;
+      doc.open();
+      doc.write(this.state.currentText);
+      doc.close();
+    }
+  }
+
+  handleClose() {
+    this.setState({isOpen: false});
   }
 
   containsTag(needle, haystack) {
@@ -46,6 +57,7 @@ class CodeBlock extends Component {
 
   tagCount(needle, haystack) {
     let count = 0;
+    if (haystack.length === 0) return 0;
     for (const h of haystack) {
       if (h.type === "Element") {
         if (h.tagName === needle) {
@@ -58,24 +70,52 @@ class CodeBlock extends Component {
     return count;
   }
 
-  onChangeText(theText) {
-    const jsonArray = himalaya.parse(this.getEditor().getValue());
-    const rulejson = JSON.parse(this.props.lesson.rulejson);
+  saveProgress(level) {
+    axios.post("/api/userprogress/save", {level}).then(resp => {
+      resp.status === 200 ? console.log("successfully saved") : console.log("error");
+    });
+  }
+
+  checkForErrors() {
+    const jsonArray = himalaya.parse(this.state.currentText);
+    const {rulejson} = this.state;
     let errors = 0;
     for (const r of rulejson) {
       if (r.type === "CONTAINS") {
         if (!this.containsTag(r.needle, jsonArray)) {
           errors++;
+          r.passing = false;
+        }
+        else {
+          r.passing = true;
         }
       }
     }
+    this.setState({isPassing: errors === 0, rulejson});
+  }
 
-    this.setState({currentText: theText, isPassing: errors === 0}, this.renderText.bind(this));
+  onChangeText(theText) {
+    this.setState({currentText: theText}, this.renderText.bind(this));
   }
 
   resetSnippet() {
     const {lesson} = this.props;
     if (lesson) this.setState({currentText: lesson.initialcontent}, this.renderText.bind(this));
+  }
+
+  getValidationBox() {
+    const {rulejson} = this.state;
+    const vList = rulejson.map(rule => {
+      if (rule.passing) {
+        return <li style={{color: "green"}}>✔ {rule.needle}</li>;
+      } 
+      else {
+        return <li style={{color: "red"}}>✗ {rule.needle}</li>;
+      }
+    });
+    return (
+      <ul>{vList}</ul>
+    );
   }
 
   verifyAndSaveCode() {
@@ -85,9 +125,18 @@ class CodeBlock extends Component {
     const lid = this.props.lesson.id;
     const name = `My ${this.props.lesson.name} Snippet`;
 
+    if (!this.state.isPassing) {
+      this.setState({isOpen: true, alertText: "Can't save a non-passing Codeblock!"});
+      return;
+    }
+
+    this.saveProgress(lid);
+
     let endpoint = "/api/snippets/";
     // todo: double check that simply having a snippet is enough to justify an update over a new,
     // given the codeblock refactor
+    
+    // todo: maybe replace this with findorupdate from userprogress?
     snippet ? endpoint += "update" : endpoint += "new";
     axios.post(endpoint, {uid, lid, name, studentcontent}).then(resp => {
       if (resp.status === 200) {
@@ -100,6 +149,8 @@ class CodeBlock extends Component {
       }
     });
   }
+
+
 
   validateHTML() {
 
@@ -123,10 +174,15 @@ class CodeBlock extends Component {
 
     if (!this.state.mounted) return <Loading />;
 
+    const validationBox = this.getValidationBox();
+
     return (
       <div id="codeBlock">
         <div className="codeBlock-head">
           { lesson.prompt }<br/><br/>
+          <div className="validation-text">
+          { validationBox }
+          </div>
           { lesson.snippet ? <Link className="share-link" to={ `/share/snippet/${lesson.snippet.id}` }>Share this Snippet</Link> : null }
         </div>
         <div className="codeBlock-body">
@@ -142,6 +198,16 @@ class CodeBlock extends Component {
             : <div className="pt-callout pt-intent-danger"><h5>Failing</h5></div>
           }
         </div>
+        <Alert
+            className="fail-alert"
+            isOpen={this.state.isOpen}
+            confirmButtonText="Okay"
+            onConfirm={this.handleClose.bind(this)}
+          >
+          <p>
+            {this.state.alertText}
+          </p>
+        </Alert>
       </div>
     );
   }

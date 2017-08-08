a4f9e612b6372426fd5f7be2b30e385630b7e051
jhmullen <jimmy.mullen@gmail.com>
2017-08-08 16:11:15 -0400

    adds selected state to quiz

--- app/components/slidetypes/Quiz.jsx
+++ app/components/slidetypes/Quiz.jsx
@@ -6,19 +6,21 @@ export default class Quiz extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOpen: false
+      isOpen: false,
+      activeQ: null
     };
   }
 
   onChooseAnswer(question) {
-    const t = Toaster.create({className: "quizToast", position: Position.TOP_CENTER});
+    const toast = Toaster.create({className: "quizToast", position: Position.TOP_CENTER});
     if (question.isCorrect) {
-      t.show({message: "That's right!", timeout: 1500, intent: Intent.SUCCESS});
+      toast.show({message: "That's right!", timeout: 1500, intent: Intent.SUCCESS});
       this.props.unblock();
     }
     else {
-      t.show({message: "Sorry, Try again!", timeout: 1500, intent: Intent.DANGER});
+      toast.show({message: "Sorry, Try again!", timeout: 1500, intent: Intent.DANGER});
     }
+    this.setState({activeQ: question.text});
   }
 
   render() {
@@ -26,15 +28,15 @@ export default class Quiz extends Component {
     const {htmlcontent1, quizjson} = this.props;
 
     let qText = htmlcontent1;
-    let qParse = [{text: "Go Check the JSON.", isCorrect: true}, {text: "I don't wanna", isCorrect: false}];
+    let qParse = [{text: "Check with", isCorrect: true}, {text: "Administrator", isCorrect: false}];
     try {
       qParse = JSON.parse(quizjson);
     } 
     catch (e) {
-      qText = "Json's Busted Yo";
+      qText = "Quiz Data is Unavailable.";
     }
 
-    const quizItems = qParse.map(question => <li className="question" key={question.text} onClick={this.onChooseAnswer.bind(this, question)}>{question.text}</li>);
+    const quizItems = qParse.map(question => <li className={this.state.activeQ === question.text ? "question quiz-selected" : "question"} key={question.text} onClick={this.onChooseAnswer.bind(this, question)}>{question.text}</li>);
 
     return (
       <div id="slide-container" className="quiz flex-row">

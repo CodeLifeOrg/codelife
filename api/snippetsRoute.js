2840f86e929feed686840e7a51cbf30315f65def
jhmullen <jimmy.mullen@gmail.com>
2017-08-09 16:50:37 -0400

    adds proper sorting by number of likes

--- api/snippetsRoute.js
+++ api/snippetsRoute.js
@@ -53,7 +53,7 @@ module.exports = function(app) {
 
   app.get("/api/snippets/allbylid", (req, res) => {
 
-    const q = "SELECT snippets.id, snippets.snippetname, snippets.studentcontent, snippets.likes, snippets.previewblob, snippets.lid, snippets.uid, users.username FROM snippets, users WHERE users.id = snippets.uid AND snippets.lid = '" + req.query.lid + "'";
+    const q = "SELECT snippets.id, snippets.snippetname, snippets.studentcontent, (select count(*) FROM likes where likes.likeid = snippets.id) AS likes, snippets.previewblob, snippets.lid, snippets.uid, users.username FROM snippets, users WHERE users.id = snippets.uid AND snippets.lid = '" + req.query.lid + "'";
     db.query(q, {type: db.QueryTypes.SELECT}).then(u => res.json(u).end());
 
     // db.snippets.findAll({where: {uid: {$not: req.user.id}, lid: req.query.lid}}).then(u => res.json(u).end());
@@ -62,14 +62,14 @@ module.exports = function(app) {
 
   app.get("/api/snippets/allothers", (req, res) => {
 
-    const q = "SELECT snippets.id, snippets.snippetname, snippets.studentcontent, snippets.likes, snippets.previewblob, snippets.lid, snippets.uid, users.username FROM snippets, users WHERE users.id = snippets.uid AND users.id != '" + req.user.id + "'";
+    const q = "SELECT snippets.id, snippets.snippetname, snippets.studentcontent, (select count(*) FROM likes where likes.likeid = snippets.id) AS likes, snippets.previewblob, snippets.lid, snippets.uid, users.username FROM snippets, users WHERE users.id = snippets.uid AND users.id != '" + req.user.id + "'";
     db.query(q, {type: db.QueryTypes.SELECT}).then(u => res.json(u).end());
 
   });
 
   app.get("/api/snippets/all", (req, res) => {
 
-    const q = "SELECT snippets.id, snippets.snippetname, snippets.studentcontent, snippets.likes, snippets.previewblob, snippets.lid, snippets.uid, users.username FROM snippets, users WHERE users.id = snippets.uid";
+    const q = "SELECT snippets.id, snippets.snippetname, snippets.studentcontent, (select count(*) FROM likes where likes.likeid = snippets.id) AS likes, snippets.previewblob, snippets.lid, snippets.uid, users.username FROM snippets, users WHERE users.id = snippets.uid";
     db.query(q, {type: db.QueryTypes.SELECT}).then(u => res.json(u).end());
 
   });

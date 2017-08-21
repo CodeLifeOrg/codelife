2c0f6b44e6c0abfdc076264b56173348f26d6e48
jhmullen <jimmy.mullen@gmail.com>
2017-08-21 16:36:23 -0400

    adds blueprint tree for content browsing

--- app/pages/LevelEditor.jsx
+++ app/pages/LevelEditor.jsx
@@ -1,6 +1,9 @@
+import axios from "axios";
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {translate} from "react-i18next";
+import {Tree} from "@blueprintjs/core";
+import Loading from "components/Loading";
 
 import "./LevelEditor.css";
 
@@ -9,21 +12,89 @@ class LevelEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mounted: false
+      mounted: false,
+      lessons: null, 
+      nodes: null
     };
   }
 
   componentDidMount() {
-    this.setState({mounted: true});
+    const lget = axios.get("/api/lessons");
+    const mlget = axios.get("/api/minilessons/all");
+    const sget = axios.get("/api/slides/all");
+    Promise.all([lget, mlget, sget]).then(resp => {
+      const lessons = resp[0].data;
+      const minilessons = resp[1].data;
+      const slides = resp[2].data;
+      lessons.sort((a, b) => a.index - b.index);
+      minilessons.sort((a, b) => a.ordering - b.ordering);
+      slides.sort((a, b) => a.ordering - b.ordering);
+      minilessons.map(m => m.slides = []);
+      lessons.map(l => l.minilessons = []);
+      for (const s of slides) {
+        const minilesson = minilessons.find(m => m.id === s.mlid);
+        if (minilesson) {
+          if (!minilesson.slides) minilesson.slides = [];
+          minilesson.slides.push(s);
+        }
+      }
+      for (const m of minilessons) {
+        const lesson = lessons.find(l => l.id === m.lid);
+        if (lesson) {
+          if (!lesson.minilessons) lesson.minilessons = [];
+          lesson.minilessons.push(m);
+        }
+      }
+      const nodes = this.buildNodes(lessons);
+      this.setState({mounted: true, lessons, nodes});
+    });
   }
+  
+  buildNodes(lessons) {
+    const ltree = [];
+    for (const l of lessons) {
+      const mltree = [];
+      for (const m of l.minilessons) {
+        const stree = [];
+        for (const s of m.slides) {
+          stree.push({id: s.id, hasCaret: false, iconName: "page-layout", label: s.title});
+        }
+        mltree.push({id: m.id, hasCaret: m.slides.length, iconName: "multi-select", label: m.name, childNodes: stree});
+      }
+      ltree.push({id: l.id, hasCaret: l.minilessons.length, iconName: "map", label: l.name, childNodes: mltree});
+    }
+    return ltree;
+  }
+
+  handleNodeClick(nodeData, _nodePath, e) {
+
+  }
+
+  handleNodeCollapse(nodeData) {
+    nodeData.isExpanded = false;
+    this.setState({nodes: this.state.nodes});
+  }
+
+  handleNodeExpand(nodeData) {
+    nodeData.isExpanded = true;
+    this.setState({nodes: this.state.nodes});
+  }
+
 
   render() {
 
-    // const {t} = this.props;
+    const {lessons, nodes} = this.state;
 
+    if (!lessons) return <Loading />;
+    
     return (
       <div id="leveleditor">
-        i will be a level editor
+        <Tree
+          onNodeClick={this.handleNodeClick.bind(this)}
+          onNodeCollapse={this.handleNodeCollapse.bind(this)}
+          onNodeExpand={this.handleNodeExpand.bind(this)}
+          contents={nodes}
+        />
       </div>
     );
   }

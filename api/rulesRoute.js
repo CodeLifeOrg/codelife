599df31579ef12d18189c1fcd75051ff468c4f35
jhmullen <jimmy.mullen@gmail.com>
2017-09-19 13:09:49 -0400

    adds new rules route

---
+++ api/rulesRoute.js
@@ -0,0 +1,15 @@
+const translate = require("../tools/translate.js");
+
+module.exports = function(app) {
+
+  const {db} = app.settings;
+
+  app.get("/api/rules", (req, res) => {
+
+    db.rules.findAll().then(u => {
+      u = translate(req.headers.host, "pt", u);
+      res.json(u).end();
+
+  });
+
+};

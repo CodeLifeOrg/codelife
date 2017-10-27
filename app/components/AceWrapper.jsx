d7c4157c23343d6fa7abb3b09500c5266ea80de4
jhmullen <jimmy.mullen@gmail.com>
2017-10-27 14:47:08 -0400

    changes tabsize to 2, closes #202

--- app/components/AceWrapper.jsx
+++ app/components/AceWrapper.jsx
@@ -8,15 +8,16 @@ export default class AceWrapper extends Component {
       require("brace/mode/html");
       require("brace/theme/idle_fingers");
       return <Ace theme="idle_fingers" width="auto" height="auto"
-                  ref={editor => this.editor = editor}
-                  showGutter={false} 
-                  wrapEnabled={false}
-                  mode="html" 
-                  setOptions={{behavioursEnabled: false}}
-                  editorProps={{
-                    $blockScrolling: Infinity
-                  }}
-                  {...this.props} />;
+        ref={editor => this.editor = editor}
+        showGutter={false} 
+        wrapEnabled={false}
+        tabSize = "2"
+        mode="html" 
+        setOptions={{behavioursEnabled: false}}
+        editorProps={{
+          $blockScrolling: Infinity
+        }}
+        {...this.props} />;
     }
     return null;
   }

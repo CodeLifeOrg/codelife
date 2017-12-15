892b893999371fe34fd9d80b151b1ae4b53bce52
jhmullen <jimmy.mullen@gmail.com>
2017-12-15 12:19:50 -0500

    further refactors codeblock api

--- api/codeBlocksRoute.js
+++ api/codeBlocksRoute.js
@@ -16,6 +16,13 @@ function flattenCodeBlock(user, cb) {
   return cb;
 }
 
+const cbIncludes = [
+  {association: "userprofile", attributes: ["sharing"]}, 
+  {association: "user", attributes: ["username"]}, 
+  {association: "likelist"}, 
+  {association: "reportlist"}
+];
+
 module.exports = function(app) {
 
   const {db} = app.settings;
@@ -23,63 +30,6 @@ module.exports = function(app) {
   // Used by Island.jsx to get this user's codeblocks
   app.get("/api/codeBlocks/mine", isAuthenticated, (req, res) => {
     db.codeblocks.findAll({where: {uid: req.user.id}}).then(u => res.json(u).end());
-  });
-
-  // Used by Home.jsx to get hand-picked featured blocks
-  app.get("/api/codeBlocks/featured", (req, res)  => {
-    db.codeblocks.findAll({
-      where: {
-        [Op.or]: [{id: 863}, {id: 834}, {id: 921}, {id: 30}]
-      },
-      include: [
-        {association: "userprofile", attributes: ["sharing"]}, 
-        {association: "user", attributes: ["username"]}, 
-        {association: "likelist"}, 
-        {association: "reportlist"}
-      ]
-    })
-      .then(cbRows => 
-        res.json(cbRows.map(cb => flattenCodeBlock(req.user, cb.toJSON()))).end()
-      );
-  });
-
-  // Used by UserCodeBlocks.jsx to get codeblock list for profile page
-  app.get("/api/codeBlocks/byuser", isAuthenticated, (req, res) => {
-    db.codeblocks.findAll({
-      include: [
-        {association: "userprofile", attributes: ["sharing"]}, 
-        {association: "user", where: {id: req.query.uid}, attributes: ["username"]}, 
-        {association: "likelist"}, 
-        {association: "reportlist"}
-      ]
-    })
-      .then(cbRows => 
-        res.json(cbRows
-          .map(cb => flattenCodeBlock(req.user, cb.toJSON()))
-          .filter(cb => !cb.hidden))
-          .end()
-      );
-  });
-
-  // Used by Share.jsx to publicly share code
-  app.get("/api/codeBlocks/byUsernameAndFilename", (req, res) => {
-    db.codeblocks.findAll({
-      where: {
-        snippetname: req.query.filename
-      },
-      include: [
-        {association: "userprofile", attributes: ["sharing"]}, 
-        {association: "user", where: {username: req.query.username}, attributes: ["username"]}, 
-        {association: "likelist"}, 
-        {association: "reportlist"}
-      ]
-    })
-      .then(cbRows => 
-        res.json(cbRows
-          .map(cb => flattenCodeBlock(req.user, cb.toJSON()))
-          .filter(cb => !cb.hidden))
-          .end()
-      );
   });
 
   // Used by Codeblock.jsx to save new Codeblocks
@@ -102,16 +52,53 @@ module.exports = function(app) {
     });
   });
 
+  // Used by Home.jsx to get hand-picked featured blocks
+  app.get("/api/codeBlocks/featured", (req, res)  => {
+    db.codeblocks.findAll({
+      where: {
+        [Op.or]: [{id: 863}, {id: 834}, {id: 921}, {id: 30}]
+      },
+      include: cbIncludes
+    })
+      .then(cbRows => 
+        res.json(cbRows.map(cb => flattenCodeBlock(req.user, cb.toJSON()))).end()
+      );
+  });
+
+  // Used by UserCodeBlocks.jsx to get codeblock list for profile page
+  app.get("/api/codeBlocks/byuser", isAuthenticated, (req, res) => {
+    db.codeblocks.findAll({
+      include: cbIncludes.map(i => i.association === "user" ? Object.assign(i, {where: {id: req.query.uid}}) : i)
+    })
+      .then(cbRows => 
+        res.json(cbRows
+          .map(cb => flattenCodeBlock(req.user, cb.toJSON()))
+          .filter(cb => !cb.hidden))
+          .end()
+      );
+  });
+
+  // Used by Share.jsx to publicly share code
+  app.get("/api/codeBlocks/byUsernameAndFilename", (req, res) => {
+    db.codeblocks.findAll({
+      where: {
+        snippetname: req.query.filename
+      },
+      include: cbIncludes.map(i => i.association === "user" ? Object.assign(i, {where: {username: req.query.username}}) : i)
+    })
+      .then(cbRows => 
+        res.json(cbRows
+          .map(cb => flattenCodeBlock(req.user, cb.toJSON()))
+          .filter(cb => !cb.hidden))
+          .end()
+      );
+  });
+
   // Used by CodeBlockList.jsx and Level.jsx to get codeblocks 
   app.get("/api/codeBlocks/all", isAuthenticated, (req, res) => {
     db.codeblocks.findAll({
       where: req.query,
-      include: [
-        {association: "userprofile", attributes: ["sharing"]}, 
-        {association: "user", attributes: ["username"]}, 
-        {association: "likelist"}, 
-        {association: "reportlist"}
-      ]
+      include: cbIncludes
     })
       .then(cbRows => 
         res.json(cbRows

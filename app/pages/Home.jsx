3d7d0221a628b9339dddb0129e68d3bfbf89f9cb
James Ferrell <hello@jamesferrell.me>
2018-03-20 12:34:41 -0400

    preps homepage for design update, adds reusable .content and .content-section classes

--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -2,12 +2,11 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router";
 import {translate} from "react-i18next";
-import {Intent, Spinner} from "@blueprintjs/core";
 import axios from "axios";
 import CodeBlockCard from "components/CodeBlockCard";
 import ProjectCard from "components/ProjectCard";
 import AuthForm from "components/AuthForm";
-import {Dialog} from "@blueprintjs/core";
+import {Dialog, Intent, Spinner} from "@blueprintjs/core";
 import "./Home.css";
 
 class Home extends Component {
@@ -57,8 +56,10 @@ class Home extends Component {
     };
 
     return (
-      <div id="Home">
-        <div id="island" className={ `island ${current ? current.theme : "island-jungle"}` }>
+      <div className="content home">
+
+        {/* OG intro */}
+        <div className={ `island content-section ${current ? current.theme : "island-jungle"}` }>
           <div className="island-image image">
             <div className="logo">
               <div className="tag">Beta</div>
@@ -70,9 +71,62 @@ class Home extends Component {
             <iframe className="video" src={ `https://www.youtube-nocookie.com/embed/${ videos[locale] || videos.en }?rel=0` } frameBorder="0" allowFullScreen></iframe>
           </div>
         </div>
-        <h3>sign up</h3>
-        <button className="pt-button pt-button-standard" onClick={this.toggleSignupOpen.bind(this, "signup")}>get started</button>
-        <button className="pt-button pt-button-standard" onClick={this.toggleSignupOpen.bind(this, "login")}>login</button>
+
+        {/* intro */}
+        { !current
+          ? <div className="intro content-section u-text-center">
+
+            <h1 className="intro-headline">{ t("Home.Headline")}</h1>
+            {/* <p className="intro-text font-lg">{ t("Home.IntroText")}</p> */}
+
+            <div className="authform-button-group u-margin-bottom-off">
+              <button className="authform-button pt-button pt-intent-primary font-md" onClick={this.toggleSignupOpen.bind(this, "signup")}>
+                { t("Home.GetStarted")}
+              </button>
+              <button className="authform-button pt-button pt-intent-primary font-md" onClick={this.toggleSignupOpen.bind(this, "login")}>
+                <span className="pt-icon pt-icon-log-in" />
+                {t("LogIn.Log_in")}
+              </button>
+            </div>
+          </div>
+          : null
+        }
+
+        {/* made on codelife */}
+        <div className="content-section">
+
+          <h2>{t("Home.MadeOnCodelife")}</h2>
+
+          {/* projects */}
+          <div className="project-section">
+            <h3>{ t("Featured Projects") }</h3>
+            <div className="project-list">
+              { !projects ? <Spinner intent={Intent.PRIMARY}/> : projects.map(p => <ProjectCard key={p.id} project={p} />) }
+            </div>
+          </div>
+
+          {/* codeblocks */}
+          <div className="codeblock-section">
+            <h3>{ t("Featured CodeBlocks") }</h3>
+            <div className="codeblock-list">
+              { !codeBlocks ? <Spinner intent={Intent.PRIMARY}/> : codeBlocks.map(c => {
+                const {theme, icon} = islands.find(i => i.id === c.lid);
+                return <CodeBlockCard key={c.id} codeBlock={c} theme={theme} icon={icon} />;
+              }) }
+            </div>
+          </div>
+        </div>
+
+        {/* about blurb */}
+        <div className="about content-section limited-width u-margin-auto">
+          <h2>{ t("What is CodeLife?") }</h2>
+          <p>{ t("splashP1") }</p>
+          <p>{ t("splashP2") }</p>
+          <p>{ t("splashP3") }</p>
+          <p>{ t("splashP4") }</p>
+        </div>
+
+        {/* Authform */}
         <Dialog
           className="form-container"
           iconName="inbox"
@@ -82,24 +136,6 @@ class Home extends Component {
         >
           <AuthForm initialMode={this.state.formMode}/>
         </Dialog>
-        <h2>{ t("Featured Projects") }</h2>
-        <div className="projects">
-          { !projects ? <Spinner intent={Intent.PRIMARY}/> : projects.map(p => <ProjectCard key={p.id} project={p} />) }
-        </div>
-        <h2>{ t("Featured CodeBlocks") }</h2>
-        <div className="codeBlocks">
-          { !codeBlocks ? <Spinner intent={Intent.PRIMARY}/> : codeBlocks.map(c => {
-            const {theme, icon} = islands.find(i => i.id === c.lid);
-            return <CodeBlockCard key={c.id} codeBlock={c} theme={theme} icon={icon} />;
-          }) }
-        </div>
-        <h2>{ t("What is CodeLife?") }</h2>
-        <div className="aboutTxt">
-          <p>{ t("splashP1") }</p>
-          <p>{ t("splashP2") }</p>
-          <p>{ t("splashP3") }</p>
-          <p>{ t("splashP4") }</p>
-        </div>
       </div>
     );
   }

48dcc2d8c6ce528d6ffb9d2272090f357f59402d
James Ferrell <hello@jamesferrell.me>
2018-03-26 18:36:14 -0400

    gently styles contest page

--- app/pages/Contest.jsx
+++ app/pages/Contest.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import {translate} from "react-i18next";
 import {Link} from "react-router";
+import {Intent} from "@blueprintjs/core";
+
 import "./Contest.css";
 
 class Contest extends Component {
@@ -8,27 +10,37 @@ class Contest extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mounted: false
+      mounted: false,
+      signedUp: false
     };
   }
 
   render() {
-    // const {t} = this.props;
+    const {t, signedUp} = this.props;
 
     return (
-      <div id="contest-container">
-        <h2>Welcome to the Codelife Contest</h2>
-        
+      <div className="content contest font-md">
+
+        <h1>Welcome to the Codelife Contest</h1>
+
         <p>code with your friends and win prizes</p>
+
         <ul>
           <li>rules</li>
           <li>rules</li>
           <li>rules</li>
         </ul>
-        <ul>
-          <li><Link to="/contest/signup">sign up</Link></li>
-          <li><Link to="/contest/submit">submit a project</Link></li>
-        </ul>
+
+        {!signedUp
+          // user has not yet signed up for the contest
+          ? <Link to="/contest/signup" className="pt-button pt-intent-primary font-md">
+            { t("Contest.GetStarted") }
+          </Link>
+          // user has signed up, and may submit a project
+          : <Link to="/contest/submit" className="pt-button pt-intent-primary font-md">
+            { t("Contest.SubmitProject") }
+          </Link>
+        }
       </div>
     );
   }

41e4dd2880602d538e6eece95b957929d88eb1d6
James Ferrell <hello@jamesferrell.me>
2018-05-09 18:25:21 -0400

    adds social links to footer; closes #429

---
+++ app/components/YoutubeIcon.svg.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class YoutubeIcon extends Component {
+  render() {
+    return (
+      <svg className="social-svg" viewBox="0 0 800 800" fill="#ff0000">
+        <path d="M400 224c144 0 201 2 224 25 17 17 26 52.125 26 151s-9 134-26 151c-23 23-80 25-224 25s-201-2-224-25c-17-17-26-52.125-26-151s9-134 26-151c23-23 80-25 224-25zm-52 100v141l135-70z"/>
+      </svg>
+    );
+  }
+}
+
+export default YoutubeIcon;

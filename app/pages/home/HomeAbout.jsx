5df36f0d9831ff05baf1ab7e238bbc2a2c25658f
James Ferrell <hello@jamesferrell.me>
2018-06-06 16:24:49 -0400

    begins styling about home page section

--- app/pages/home/HomeAbout.jsx
+++ app/pages/home/HomeAbout.jsx
@@ -14,30 +14,35 @@ class HomeAbout extends Component {
     };
 
     return (
-      <div className="content-section">
-        {/* video */}
-        {/* container needed to make it responsive */}
-        <div className="video-container">
-          <iframe className="video-iframe"
-            src={ `https://www.youtube-nocookie.com/embed/${ videos[locale] || videos.en }?rel=0` }
-            frameBorder="0"
-            allowFullScreen />
-        </div>
+      <div className="content-section home-about-section">
+        <div className="home-about-half-container">
 
-        {/* about blurb */}
-        <div className="about content-section limited-width">
+          {/* about blurb */}
+          <div className="home-about-half">
 
-          <h2 className="font-xl">{ t("Home.AboutHeading") }</h2>
+            <h2 className="font-xl">{ t("Home.AboutHeading") }</h2>
 
-          <p className="font-md">{ t("Home.AboutParagraph1") }</p>
-          <p className="font-md">{ t("Home.AboutParagraph2") }</p>
-          <p className="font-md">{ t("Home.AboutParagraph3") }</p>
+            <p className="font-md">{ t("Home.AboutParagraph1") }</p>
+            <p className="font-md">{ t("Home.AboutParagraph2") }</p>
+            <p className="font-md">{ t("Home.AboutParagraph3") }</p>
 
-          {/* about page link */}
-          <Link to="/about" className="button font-md">
-            { t("Home.AboutButton") }
-          </Link>
+            {/* about page link */}
+            <Link to="/about" className="button font-md">
+              { t("Home.AboutButton") }
+            </Link>
+          </div>
 
+          {/* video */}
+          <div className="home-about-half">
+
+            {/* container needed to make it responsive */}
+            <div className="video-container">
+              <iframe className="video-iframe"
+                src={ `https://www.youtube-nocookie.com/embed/${ videos[locale] || videos.en }?rel=0` }
+                frameBorder="0"
+                allowFullScreen />
+            </div>
+          </div>
         </div>
       </div>
     );

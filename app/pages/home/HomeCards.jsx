3fda618b243c661cbbdd05e7c43290a7e3da11c4
James Ferrell <hello@jamesferrell.me>
2018-06-06 15:57:47 -0400

    styles home page cards section

--- app/pages/home/HomeCards.jsx
+++ app/pages/home/HomeCards.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {Link} from "react-router";
 import {translate} from "react-i18next";
+import {Tooltip2} from "@blueprintjs/labs";
 import LoadingSpinner from "components/LoadingSpinner";
 import CodeBlockCard from "components/CodeBlockCard";
 import ProjectCard from "components/ProjectCard";
@@ -12,14 +13,19 @@ class HomeCards extends Component {
     const {codeBlocks, islands, projects, t} = this.props;
 
     return (
-      <div className="content-section">
+      <div className="content-section cards-section">
 
         {/* keep this a paragraph so that project and codeblock cards have the right heading level */}
-        <p className="heading font-xl">{t("Home.MadeOnCodelife")}</p>
+        <p className="cards-heading heading font-xl">{t("Home.MadeOnCodelife")}</p>
 
         {/* projects */}
         <div className="project-section u-clearfix">
-          <h2 className="projects-heading">{ t("Projects") }</h2>
+          <h2 className="heading projects-heading">
+            { t("Projects") }
+            <Tooltip2 portalClassName="heading-explainer-tooltip" content={ t("ProjectExplainer")} defaultIsOpen={true} inheritDarkTheme={false}>
+              <span className="heading-explainer pt-icon pt-icon-help" />
+            </Tooltip2>
+          </h2>
           <p className="u-visually-hidden">{ t("ProjectExplainer")}</p>
           <div className="card-list project-list">
             { !projects ? <LoadingSpinner /> : projects.map(p => <ProjectCard key={p.id} project={p} />) }
@@ -28,7 +34,12 @@ class HomeCards extends Component {
 
         {/* codeblocks */}
         <div className="codeblock-section u-clearfix">
-          <h2 className="codeblocks-heading">{ t("CodeBlocks") }</h2>
+          <h2 className="heading codeblocks-heading">
+            { t("CodeBlocks") }
+            <Tooltip2 portalClassName="heading-explainer-tooltip" content={ t("CodeblockExplainer")}>
+              <span className="heading-explainer pt-icon pt-icon-help" />
+            </Tooltip2>
+          </h2>
           <p className="u-visually-hidden">{ t("CodeblockExplainer")}</p>
           <div className="card-list codeblock-list">
             { !codeBlocks ? <LoadingSpinner /> : codeBlocks.map(c => {

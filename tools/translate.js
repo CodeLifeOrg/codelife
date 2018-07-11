9837d08f7736963d13316a2a029e25af44dde47c
jhmullen <jimmy.mullen@gmail.com>
2018-07-11 16:02:08 -0400

    documents tools directory

--- tools/translate.js
+++ tools/translate.js
@@ -1,3 +1,10 @@
+/**
+ * When requests come in to the Codelife API, their locale/language comes in as well.
+ * The columns in the islands/levels/slides tables are structured such that any given column,
+ * such as title, has a sister column lang_title, in this case, pt_title. In the object that
+ * is about to be returned to the requester, pave all of the normal columns with the content
+ * of the pt_column (if one exists). 
+ */
 module.exports = function(host, lang, resp) {
   if (host.includes(`${lang}.`)) {
     for (const r of resp) {
